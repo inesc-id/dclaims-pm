5d60095b00c70f0b0a847db4066a9de2eb730053
Joao Santos <jrmsantos15@gmail.com>
2017-10-25 18:48:19 +0100

    day's work

--- building_blocks/demos/clickbait/client/website/js/clickbait-api.js
+++ building_blocks/demos/clickbait/client/website/js/clickbait-api.js
@@ -1,6 +1,6 @@
 document.getElementById('jsonid2').style.width="800px";
 var serverAddress = "http://146.193.41.153:8091"
-function sendMessage(name){
+function sendMessage_old(name){
     var jsonfile = jQuery.parseJSON(name);
     var data = JSON.stringify(jsonfile);
 
@@ -18,7 +18,7 @@ function sendMessage(name){
     xhr.send(data);
 }
 
-function sendGetMessage(name){
+function sendMessage(name){
     var data = null;
 
 
@@ -31,7 +31,7 @@ function sendGetMessage(name){
         }
     });
 //application/javascript
-    xhr.open("GET", serverAddress+"/verify?ipfsAddr="+name);
+    xhr.open("GET", serverAddress+"/test?test_field="+name+"&second_field="+"blabla");
     xhr.setRequestHeader("content-type", "application/javascript");
 
     //xhr.setRequestHeader("postman-token", "9478c587-f2da-2c03-fe1a-5747306ae18f");
--- building_blocks/demos/clickbait/server/routes/routes.js
+++ building_blocks/demos/clickbait/server/routes/routes.js
@@ -1,11 +1,97 @@
+var storage = require('node-persist');
+var arraylist = require('arraylist')
 
+
+function getClaimsList(url){
+    return new Promise(function(fulfill,reject){
+        storage.init().then(function(){
+            storage.getItem(url).then(function(article){
+                if(article) {
+                    fulfill(article)
+                }
+                else{
+                    fulfill(false)
+                }
+            })
+        })
+    })
+
+
+}
+
+function appendToClaimsList(claimsList, newClaim, key){
+    return new Promise(function(fulfill,reject){
+        claimsList.add(newClaim);
+
+        storage.init( ).then(function() {
+            storage.setItem(key,claimsList)
+                .then(function() {
+                    console.log("[sucess] added to existing list")
+                    fulfill("[sucess] added to existing list")
+                })
+
+        });
+
+    })
+
+}
+
+function createNewClaimsList(newClaim, key){
+    return new Promise(function(fulfill,reject){
+
+        storage.init( ).then(function() {
+            var list = new ArrayList()
+            list.add(vc)
+            console.log("cena "+list)
+            storage.setItem(key,list)
+                .then(function() {
+                    console.log("[success] created new list")
+                    fulfill("[success] created new list")
+                })
+        });
+    })
+}
 
 var appRouter = function(app) {
 
-    app.get('/test', function(req,res){
-        var field = req.query.test_field
+    app.get('/verify', function(req,res){
+        var req_field = req.query.claim
+        var req_url = req.query.article
+        var req_ip = req.ip
 
-        res.send("field")
+        var coolPromise = null
+
+        var vc = {claim:req_field,
+                  url: req_url,
+                  ip: req_ip}
+
+        getClaimsList(req_url).then(function(value){
+            if(value){
+                appendToClaimsList(value,vc,req_url)
+            }else{
+                console.log("not here... Adding...")
+                createNewClaimsList(vc,req_url)
+
+            }
+        }).then(function(value){
+            res.send("All good.")
+        })
+
+       /* storage.init( ).then(function() {
+            //then start using it
+            storage.setItem(req_url,vc)
+                .then(function() {
+
+                    return storage.getItem(req_url)
+                })
+                .then(function(value) {
+
+                    console.log(value); // yourname
+                    res.send("Claim < "+req_field+" > From < "+req_ip+" >"+" in <"+req_url+" >")
+                })
+        });*/
+
+
 
     })
 }
